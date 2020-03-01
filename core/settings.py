bfddcacfe87bfaf19f3eafc897d33c979d6dab7c
MurungaKibaara <murungakibaara@outlook.com>
2020-03-01 19:41:36 +0300

    [Add user roles to custom model]

--- core/settings.py
+++ core/settings.py
@@ -120,6 +120,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            # 'ENGINE': 'django.db.backends.sqlite3',
+            # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'eretail',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     '',
+            'PORT':     '',
+        }
+    }
+
 
 DATABASES = {
     'default': {
